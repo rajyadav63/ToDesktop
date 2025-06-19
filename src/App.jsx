b77ffe97969a68eb554d10320d165c2e31f9cbbd
rajyadav63 <vector36razz@gmail.com>
2025-06-19 17:50:36 +0530

    Step3 is done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import AppSlider from './components/AppSlider'
 import Hero from './components/Hero'
 import Nabvar from './components/Nabvar'
+import Step1 from './components/works-step/Step1'
+import Step2 from './components/works-step/Step2'
+import Step3 from './components/works-step/Step3'
 
 function App() {
 
@@ -11,7 +14,10 @@ function App() {
       <Nabvar />
       <Hero />
       <AppSlider />
-      <h1 className='text-4xl font-semibold  m-[40px]'>How it works</h1>
+      <h1 className='text-4xl lg:text-5xl font-semibold m-[20px] lg:ml-[100px] mt-[40px]'>How it works</h1>
+      <Step1 />
+      <Step2 />
+      <Step3 />
       <div className='h-[1000px]'></div>
     </>
   )
--- src/components/works-step/Step3.jsx
+++ src/components/works-step/Step3.jsx
@@ -6,23 +6,26 @@ const Step3 = () => {
             <div className='flex flex-col lg:flex-row  border border-blue-200 rounded-lg h-screen lg:ml-[100px] lg:mr-[90px] lg:mt-9'>
                 <div className='flex flex-col  lg:max-w-[700px] lg:m-[40px]'>
                     <h1 className='m-5 border border-yellow-600 w-fit px-4 py-1 rounded-lg bg-yellow-100 font-medium'>Step3</h1>
-                    <h3 className='ml-5 mt-2 mr-4 mb-2 text-4xl font-semibold sm:text-3xl lg:text-6xl lg:mt-[50px]'>Bootstrap straight from your web app</h3>
-                    <p className='ml-5 mr-5 lg:mt-[30px] lg:max-w-[550px]'>Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
-                    <div className='flex md:items-center gap-4 m-4 lg:mt-[25px]'>
+                    <h3 className='ml-5 mt-2 mr-4 mb-2 text-4xl font-semibold sm:text-3xl lg:text-6xl lg:mt-[50px]'>Publish  🚀</h3>
+                    <p className='ml-5 mr-5 lg:mt-[30px] lg:max-w-[550px]'>One-click will publish your desktop app to your customers and give you a download link to put on your website.</p>
+                    <div className='flex md:items-center gap-4 mt-7 ml-5 mr-5 lg:mt-[30px]'>
                         <div className='w-60 flex flex-col gap-2 underline text-blue-600'>
-                            <a href="#"><span>✔️</span>Multiple windows</a>
-                            <a href="#"><span>✔️</span>Menubar/tray menus</a>
-                            <a href="#"><span>✔️</span>Offline support</a>
+                            <a href="#"><span>✔️</span>Super-fast global CDN</a>
+                            <a href="#"><span>✔️</span>Magic universal links</a>
+                            <a href="#"><span>✔️</span>Manage version numbers</a>
                         </div>
                         <div className='w-60 flex flex-col gap-2 underline text-blue-600'>
-                            <a href="#"><span>✔️</span>Customizable menu</a>
-                            <a href="#"><span>✔️</span>Lunch on startup</a>
-                            <a href="#"><span>✔️</span>Tabs (Mac only)</a>
+                            <a href="#"><span>✔️</span>Download links on your domain</a>
+                            <a href="#"><span>✔️</span>Download analytics</a>
+                            <a href="#"><span>✔️</span>Native installers for all platforms</a>
                         </div>
                     </div>
                 </div>
-                <div className='mt-2 lg:mt-[50px] flex justify-center items-center'>
-                    <img className='w-fit lg:h-[700px] md:h-[400px]' src="/assets/asset 66.svg" />
+                <div className=' relative m-2 lg:mt-[50px] flex justify-center items-center'>
+                    <img className='m-3 rounded-xl h-[350px] lg:h-[700px] md:h-[400px]' src="/assets/asset 57.svg" />
+                    <div className=' absolute text-center font-extralight  h-[40px] w-[160px] rounded-xl m-5 bg-gray-400'>
+                        <p>Install App</p>
+                    </div>
                 </div>
             </div>
         </div>
