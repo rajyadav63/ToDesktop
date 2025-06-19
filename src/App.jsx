91d5cca5bb25862f424929759b23f2ac680999dc
rajyadav63 <vector36razz@gmail.com>
2025-06-19 18:39:17 +0530

    ToDesktop handle details

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import AppSlider from './components/AppSlider'
 import Hero from './components/Hero'
 import Nabvar from './components/Nabvar'
+import ToDesktopHandle from './components/ToDesktopHandle'
 import Step1 from './components/works-step/Step1'
 import Step2 from './components/works-step/Step2'
 import Step3 from './components/works-step/Step3'
@@ -18,6 +19,9 @@ function App() {
       <Step1 />
       <Step2 />
       <Step3 />
+      <h1 className='max-w-[300px] md:max-w-[500px] text-5xl font-medium lg:font-semibold m-[20px] lg:ml-[100px] mt-[40px]'>ToDesktop handles
+        the details</h1>
+        <ToDesktopHandle />
       <div className='h-[1000px]'></div>
     </>
   )
---
+++ src/components/ToDesktopHandle.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const ToDesktopHandle = () => {
+  return (
+    <div className='flex flex-col lg:flex-row m-8 gap-5'>
+      <div className='lg:w-1/3'>
+        <div className='bg-gray-200 border border-gray-400 flex flex-col justify-center items-center h-auto py-4 rounded-2xl mb-5'>
+          <h1 className='text-2xl mb-1 font-semibold'>Native Notifications</h1>
+          <img src="/assets/asset 37.png" />
+        </div>
+        <div className='bg-gray-200 border border-gray-400 flex flex-col justify-center items-center h-auto py-4 rounded-2xl mb-5'>
+          <h1 className='text-2xl mb-1 font-semibold'>Auto updates</h1>
+          <p className='text-center m-3'>We’ll ensure the underlying browser is up to date and deliver performance improvements, security patches, & additional features.</p>
+          <img src="/assets/asset 38.png" />
+        </div>
+      </div>
+      <div className='lg:w-1/3'>
+        <div className='bg-gray-200 border border-gray-400 flex flex-col justify-center items-center h-auto py-4 rounded-2xl mb-5'>
+          <h1 className='text-2xl mb-1 font-semibold'>Plugins</h1>
+          <img src="/assets/asset 39.png" />
+        </div>
+        <div className='bg-gray-200 border border-gray-400 flex flex-col justify-center items-center h-auto py-4 rounded-2xl mb-5'>
+          <h1 className='text-2xl mb-1 font-semibold'>Access to Native APIs</h1>
+          <p className='text-center m-2'>ToDesktop ensures the underlying browser, performance improvements, security patches and additional features are always up to date.</p>
+          <img src="/assets/asset 40.png" />
+        </div>
+      </div>
+      <div className='lg:w-1/3 gap-2'>
+        <div className='bg-gray-200 border border-gray-400 flex flex-col justify-center items-center h-auto py-4 rounded-2xl mb-5'>
+          <h1 className='text-2xl mb-1 font-semibold'>Native Notifications</h1>
+          <img src="/assets/asset 41.png" />
+        </div>
+        <div className='bg-gray-200 border border-gray-400 flex flex-col justify-center items-center h-auto py-4 rounded-2xl mb-5'>
+          <h1 className='text-2xl mb-1 font-semibold'>Native Installers</h1>
+          <p className='text-center m-2'>We even provide a magic link which will detect your users operating system & download the most up to date version of your app.</p>
+          <img src="/assets/asset 43.png" />
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default ToDesktopHandle
