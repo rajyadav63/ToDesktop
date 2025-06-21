dccb8976aa48cdae2acba70aac215db7a293b74a
rajyadav63 <vector36razz@gmail.com>
2025-06-21 10:48:37 +0530

    Footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='border border-slate-300 h-auto w-[90vw] py-12 m-6 rounded-xl flex flex-col lg:flex-row justify-center lg:justify-around items-center'>
-            <div className='flex items-center text-center gap-1 mt-5 lg:mt-0'>
+        <div className='border border-slate-300 h-auto w-[90vw] py-12 m-6 rounded-xl flex flex-col lg:flex-row justify-center lg:justify-around items-center gap-5'>
+            <div className='flex items-center text-center gap-1'>
                 <img src="assets/asset 0.png" />
                 <span className='font-medium'>ToDesktop</span>
             </div>
-            <div className='mg-12 lg:mb-0 flex justify-center items-center text-center'>
+            <div className='mt-8 lg:mt-0 mb-5 lg:mb-0 flex justify-center items-center text-center'>
                 <a href="#">Documentaton</a>
             </div>
             <div className='flex justify-between items-center mt-5 lg:mt-0  px-5  gap-5'>
