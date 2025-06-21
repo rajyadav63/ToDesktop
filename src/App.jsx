944e84bfd8a20e62ab514edabdbcd106d7f4db89
rajyadav63 <vector36razz@gmail.com>
2025-06-21 11:04:05 +0530

    app jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
       <h1 className='text-5xl font-medium lg:font-semibold m-[20px] lg:ml-[100px] mt-[40px]'>Customer Stories</h1>
       <CustomerStories />
       <DownloadBuilder />
-      <h1 className='max-w-[500px] lg:max-w-[700px]  text-5xl font-medium m-8 mt-24'>Choose a plan that fits your needs</h1>
+      <h1 className='max-w-[500px] lg:max-w-[700px]  text-5xl font-medium m-8 mt-24 lg:ml-16'>Choose a plan that fits your needs</h1>
       <Pricing />
       <SupportCard />
       <h1 className='text-3xl lg:text-5xl text-center font-semibold lg:font-bold m-[20px] lg:ml-[100px] mt-[40px]'>Frequently Asked Questions</h1>
